14c1e03a3c60ca12da965236a7f65b4a2d3c6af6
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-10 06:09:01 -0400

    Added geojson, added team's division to txt file, created styling to use different marker colors for each division, created iframe to show data for each arena

--- nba_arenas_map.py
+++ nba_arenas_map.py
@@ -9,6 +9,36 @@ import pandas
 data = pandas.read_csv('nba-arenas.txt')
 latitude = list(data['LAT'])
 longitude = list(data['LON'])
+team = list(data['TEAM'])
+arena = list(data['ARENA'])
+capacity = list(data['CAPACITY'])
+year_opened = list(data['OPENED'])
+division = list(data['DIVISION'])
+
+arena_info = [
+    latitude, longitude, team, arena, capacity, year_opened, division
+]
+
+
+def color_producer(division):
+    return {
+        'Atlantic': 'red',
+        'Central': 'green',
+        'Southeast': 'beige',
+        'Pacific': 'orange',
+        'Southwest': 'lightgray',
+        'Northwest': 'purple'
+    }[division]
+
+
+html = """
+<div style="text-align: center">
+<h3 style="color: #4D9078">%s</h3>
+<h4 style="color: #f78154">%s</h4>
+<h5 style="color: #666">Max Capacity of %s</h5>
+<h5 style="color: #666">Opened in %s</h5>
+</div>
+"""
 
 # Folium.Map --> generates a base `Map` object that can be passed
 # many different parameters
@@ -22,12 +52,18 @@ fg = folium.FeatureGroup(name="NBA Arenas Map")
 
 # Between Map object and save method you can add elements to the Feature Group
 # Loop through text file and add marker at location
-for lat, lon in zip(latitude, longitude):
+for lat, lon, team, arena, year, capacity, division in zip(*arena_info):
+    iframe = folium.IFrame(
+        html=html % (arena, team, str(year), str(capacity)),
+        width=235,
+        height=145)
     fg.add_child(
         folium.Marker(
             location=[lat, lon],
-            popup='Hi I am a marker',
-            icon=folium.Icon('green')))
+            popup=folium.Popup(iframe),
+            icon=folium.Icon(color_producer(division))))
+
+fg.add_child(folium.GeoJson(open('world.json', encoding='utf-8-sig').read()))
 
 # Add the feature group to the map as a child element
 map.add_child(fg)
